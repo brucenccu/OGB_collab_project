cbd8dcaf0dd57558b733422e6751e388a7096311
brucenccu <sofar106703046@gmail.com>
2021-09-09 14:44:39 +0800

    rename hpe_node_embed_src

--- download.sh
+++ download.sh
@@ -7,7 +7,8 @@ mv ./hpe_node_embed_src/cli_Makefile ./Makefile
 mv ./Makefile ./smore/cli
 
 mv ./hpe_node_embed_src/HPE_node_embed* ./smore/src/model
-mv ./hpe_node_embed_src/hpe_node_embed.cpp ./smore/cli
+mv ./hpe_node_embed_src/cli_hpe_node_embed.cpp ./smore/cli
+mv ./smore/cli/cli_hpe_node_embed.cpp ./smore/cli/hpe_node_embed.cpp
 
 rm -rf hpe_node_embed_src
 
