48983373d3338cdd650cc69691b14606fd081c61
brucenccu <sofar106703046@gmail.com>
2021-10-14 14:32:21 +0800

    add dataset

--- download.sh
+++ download.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-#mv ./node_embed_src/src_Makefile ./Makefile
-#mv ./Makefile ./smore/src/
+echo -e "Compile the SMORe ......"
 cp ./node_embed_src/src_Makefile ./smore/src/Makefile
-
-#mv ./node_embed_src/cli_Makefile ./Makefile
-#mv ./Makefile ./smore/cli
 cp ./node_embed_src/cli_Makefile ./smore/cli/Makefile
 
-#mv ./node_embed_src/BPR_node_embed* ./smore/src/model
-#mv ./node_embed_src/cli_bpr_node_embed.cpp ./smore/cli
-#mv ./smore/cli/cli_bpr_node_embed.cpp ./smore/cli/bpr_node_embed.cpp
-#cp ./node_embed_src/cli_bpr_node_embed.cpp ./smore/cli
 cp ./node_embed_src/BPR_node_embed.cpp ./smore/src/model/BPR_node_embed.cpp
 cp ./node_embed_src/BPR_node_embed.h ./smore/src/model/BPR_node_embed.h
 cp ./node_embed_src/cli_bpr_node_embed.cpp ./smore/cli/bpr_node_embed.cpp
@@ -27,19 +19,12 @@ cp ./node_embed_src/cli_warp_node_embed.cpp ./smore/cli/warp_node_embed.cpp
 cp ./node_embed_src/HBPR_node_embed.cpp ./smore/src/model/HBPR_node_embed.cpp
 cp ./node_embed_src/HBPR_node_embed.h ./smore/src/model/HBPR_node_embed.h
 cp ./node_embed_src/cli_hoprec_node_embed.cpp ./smore/cli/hoprec_node_embed.cpp
-#mv ./node_embed_src/HPE_node_embed* ./smore/src/model
-#mv ./node_embed_src/cli_hpe_node_embed.cpp ./smore/cli
-#mv ./smore/cli/cli_hpe_node_embed.cpp ./smore/cli/hpe_node_embed.cpp
-
-#mv ./node_embed_src/WARP_node_embed* ./smore/src/model
-#mv ./node_embed_src/cli_warp_node_embed.cpp ./smore/cli
-#mv ./smore/cli/cli_warp_node_embed.cpp ./smore/cli/warp_node_embed.cpp
-
-#mv ./node_embed_src/HBPR_node_embed* ./smore/src/model
-#mv ./node_embed_src/cli_hoprec_node_embed.cpp ./smore/cli
-#mv ./smore/cli/cli_hoprec_node_embed.cpp ./smore/cli/hoprec_node_embed.cpp
-
-#rm -rf node_embed_src
 
 cd smore
 make
+
+echo -e "Download the dataset ..."
+wget -O ./data.tar.gz "https://www.dropbox.com/s/snkpt7aihmesaws/data.tar.gz?dl=0"
+tar zxvf ./data.tar.gz
+
+echo -e "Download Successfully!"
